d866eb17380c4762128124749a05e85f85dc3cbb
herowzz <herowzz@qq.com>
2018-02-25 16:53:14 +0800

    加入for update lock查询

--- src/main/java/com/github/herowzz/springfuse/data/jpa/dao/IBaseDao.java
+++ src/main/java/com/github/herowzz/springfuse/data/jpa/dao/IBaseDao.java
@@ -2,9 +2,14 @@ package com.github.herowzz.springfuse.data.jpa.dao;
 
 import java.io.Serializable;
 
+import javax.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
 
 /**
  * BaseDao extends spring data interface.<br>
@@ -17,5 +22,15 @@ import org.springframework.data.repository.NoRepositoryBean;
  */
 @NoRepositoryBean
 public interface IBaseDao<T, PK extends Serializable> extends JpaRepository<T, PK>, JpaSpecificationExecutor<T> {
+
+	/**
+	 * 根据主键带锁查询实体(for update)<br>
+	 * service层调用时需加上{@link javax.transaction.Transactional}注解,事物结束之后将自动释放锁
+	 * @param ID 主键
+	 * @return 实体(没有则返回null)
+	 */
+	@Lock(value = LockModeType.PESSIMISTIC_WRITE)
+	@Query("select t from #{#entityName} t where t.id = :ID")
+	public T findByIdForUpdate(@Param("ID") PK ID);
 
 }
