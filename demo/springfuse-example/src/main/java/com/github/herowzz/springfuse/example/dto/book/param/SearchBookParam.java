8bfabe8643ba67e132f9f5549bbceb22741ba62e
herowzz <herowzz@qq.com>
2018-07-04 17:36:40 +0800

    add query null process

--- demo/springfuse-example/src/main/java/com/github/herowzz/springfuse/example/dto/book/param/SearchBookParam.java
+++ demo/springfuse-example/src/main/java/com/github/herowzz/springfuse/example/dto/book/param/SearchBookParam.java
@@ -1,7 +1,10 @@
 package com.github.herowzz.springfuse.example.dto.book.param;
 
+import org.springframework.util.StringUtils;
+
 import com.github.herowzz.springfuse.example.domain.QBook;
 import com.github.herowzz.springfuse.example.domain.refrence.BookTypeEnum;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 
 public class SearchBookParam {
@@ -12,8 +15,14 @@ public class SearchBookParam {
 
 	public Predicate build() {
 		QBook book = QBook.book;
-		Predicate pred = book.name.like(name + "%").and(book.bookType.eq(bookType));
-		return pred;
+		BooleanBuilder qb = new BooleanBuilder();
+		if (StringUtils.hasText(name)) {
+			qb.and(book.name.like(name + "%"));
+		}
+		if (bookType != null) {
+			qb.and(book.bookType.eq(bookType));
+		}
+		return qb;
 	}
 
 }
