29d8e99acd2d3a3cc2205ee58cf3761b26bed3f7
herowzz <herowzz@qq.com>
2018-07-11 16:03:24 +0800

    request error add log record and param error append detail message.

--- src/main/java/com/github/herowzz/springfuse/api/handler/GlobalHandler.java
+++ src/main/java/com/github/herowzz/springfuse/api/handler/GlobalHandler.java
@@ -1,5 +1,7 @@
 package com.github.herowzz.springfuse.api.handler;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.github.herowzz.springfuse.api.dto.ApiResult;
 import com.github.herowzz.springfuse.api.dto.refrence.ApiResultCodeEnum;
@@ -48,13 +51,25 @@ public class GlobalHandler {
 
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	public ResponseEntity<ApiResult> HttpMessageNotReadableException(HttpServletRequest request, HttpMessageNotReadableException ex) {
+		logger.error("Rest request error! URL: " + request.getRequestURI(), ex);
 		ApiResult dto = ApiResult.paramError();
-		dto.msg += " 请求数据无法转换成正确格式. (" + ex.getLocalizedMessage() + ")";
+		if(ex.getCause() instanceof InvalidFormatException) {
+			dto.msg += " 请求参数不正确. (";
+			InvalidFormatException ifExp = (InvalidFormatException)ex.getCause();
+			List<Reference> refList = ifExp.getPath();
+			for (Reference ref : refList) {
+				dto.msg += "参数: " + ref.getFieldName() + " 应该是 " + ifExp.getTargetType().getSimpleName() + "类型. ";
+			}
+			dto.msg += ")";
+		} else {
+			dto.msg += " 请求数据无法转换成正确格式. (" + ex.getLocalizedMessage() + ")";
+		}
 		return new ResponseEntity<ApiResult>(dto, HttpStatus.OK);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ApiResult> MethodArgumentNotValidException(HttpServletRequest request, MethodArgumentNotValidException ex) {
+		logger.error("Rest request error! URL: " + request.getRequestURI(), ex);
 		ApiResult dto = ApiResult.paramError();
 		StringBuilder sb = new StringBuilder();
 		sb.append(dto.msg).append("(");
@@ -70,6 +85,7 @@ public class GlobalHandler {
 
 	@ExceptionHandler(MissingServletRequestParameterException.class)
 	public ResponseEntity<ApiResult> MissingServletRequestParameterException(HttpServletRequest request, MissingServletRequestParameterException ex) {
+		logger.error("Rest request error! URL: " + request.getRequestURI(), ex);
 		ApiResult dto = ApiResult.paramError();
 		dto.msg = ((MissingServletRequestParameterException) ex).getMessage();
 		return new ResponseEntity<ApiResult>(dto, HttpStatus.OK);
@@ -77,6 +93,7 @@ public class GlobalHandler {
 
 	@ExceptionHandler(ServiceException.class)
 	public ResponseEntity<ApiResult> ServiceException(HttpServletRequest request, ServiceException ex) {
+		logger.error("Rest request error! URL: " + request.getRequestURI(), ex);
 		ApiResult dto = new ApiResult();
 		dto.msg = ((ServiceException) ex).getMessage();
 		dto.code = ((ServiceException) ex).getCode();
@@ -93,6 +110,7 @@ public class GlobalHandler {
 
 	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
 	public ResponseEntity<ApiResult> HttpRequestMethodNotSupportedException2(HttpServletRequest request, HttpMediaTypeNotSupportedException ex) {
+		logger.error("Rest request error! URL: " + request.getRequestURI(), ex);
 		ApiResult dto = new ApiResult();
 		dto.code = ApiResultCodeEnum.HTTP_MEDIATYPE_NOT_SUPPORT.code;
 		dto.msg = ApiResultCodeEnum.HTTP_MEDIATYPE_NOT_SUPPORT.msg + "(" + ex.getMessage() + ")";
