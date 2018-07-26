a66632be78a9fdf04e9f2896f1ca197faf2922c1
herowzz <herowzz@qq.com>
2018-07-26 15:53:29 +0800

    update...

---
+++ src/main/java/com/github/herowzz/springfuse/data/jpa/doc/IGenerateDoc.java
@@ -0,0 +1,7 @@
+package com.github.herowzz.springfuse.data.jpa.doc;
+
+public interface IGenerateDoc {
+
+	void export(String entityPackage, String exportPath);
+
+}
---
+++ src/main/java/com/github/herowzz/springfuse/data/jpa/doc/impl/GenerateHtmlDoc.java
@@ -0,0 +1,12 @@
+package com.github.herowzz.springfuse.data.jpa.doc.impl;
+
+import com.github.herowzz.springfuse.data.jpa.doc.IGenerateDoc;
+
+public class GenerateHtmlDoc implements IGenerateDoc {
+
+	@Override
+	public void export(String entityPackage, String exportPath) {
+		// TODO need implement
+	}
+
+}
