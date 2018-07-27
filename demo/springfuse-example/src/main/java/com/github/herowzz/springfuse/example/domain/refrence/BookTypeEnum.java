a1c090f3edc6bdfd2f8472f12f3438984b5c2e79
herowzz <herowzz@qq.com>
2018-07-27 17:34:28 +0800

    custom enum implements IBaseEnum

--- demo/springfuse-example/src/main/java/com/github/herowzz/springfuse/example/domain/refrence/BookTypeEnum.java
+++ demo/springfuse-example/src/main/java/com/github/herowzz/springfuse/example/domain/refrence/BookTypeEnum.java
@@ -1,11 +1,43 @@
 package com.github.herowzz.springfuse.example.domain.refrence;
 
-public enum BookTypeEnum {
+import com.github.herowzz.springfuse.core.bean.enumtype.IBaseEnum;
 
-	LITERATURE,
+public enum BookTypeEnum implements IBaseEnum {
 
-	POLITICAL,
+	LITERATURE("文学"),
 
-	ECONOMIC
+	POLITICAL("政治"),
+
+	ECONOMIC("经济");
+
+	private String title;
+
+	private BookTypeEnum(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String getTitle() {
+		return this.title;
+	}
+
+	@Override
+	public String toString() {
+		return this.getTitle();
+	}
+
+	public static BookTypeEnum get(int index) {
+		return BookTypeEnum.values()[index];
+	}
+
+	@Override
+	public String getName() {
+		return this.name();
+	}
+
+	@Override
+	public int getValue() {
+		return this.ordinal();
+	}
 
 }
--- demo/springfuse-example/src/main/java/com/github/herowzz/springfuse/example/domain/refrence/PermissionTypeEnum.java
+++ demo/springfuse-example/src/main/java/com/github/herowzz/springfuse/example/domain/refrence/PermissionTypeEnum.java
@@ -1,19 +1,51 @@
 package com.github.herowzz.springfuse.example.domain.refrence;
 
+import com.github.herowzz.springfuse.core.bean.enumtype.IBaseEnum;
+
 /**
  * 权限类型
  * @author wangzz
  */
-public enum PermissionTypeEnum {
+public enum PermissionTypeEnum implements IBaseEnum {
 
 	/**
 	 * 模块
 	 */
-	MODULE,
+	MODULE("模块"),
 
 	/**
 	 * 按钮
 	 */
-	BUTTON
+	BUTTON("按钮");
+
+	private String title;
+
+	private PermissionTypeEnum(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String getTitle() {
+		return this.title;
+	}
+
+	@Override
+	public String toString() {
+		return this.getTitle();
+	}
+
+	public static PermissionTypeEnum get(int index) {
+		return PermissionTypeEnum.values()[index];
+	}
+
+	@Override
+	public String getName() {
+		return this.name();
+	}
+
+	@Override
+	public int getValue() {
+		return this.ordinal();
+	}
 
 }
