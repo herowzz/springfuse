9b483755c42a243203b880a7175c62b267d638c3
herowzz <herowzz@qq.com>
2019-04-10 13:43:28 +0800

    add random date

--- src/main/java/com/github/herowzz/springfuse/api/mock/LocalDateMock.java
+++ src/main/java/com/github/herowzz/springfuse/api/mock/LocalDateMock.java
@@ -3,13 +3,20 @@ package com.github.herowzz.springfuse.api.mock;
 import java.time.LocalDate;
 
 import com.github.jsonzou.jmockdata.DataConfig;
+import com.github.jsonzou.jmockdata.JMockData;
+import com.github.jsonzou.jmockdata.MockConfig;
 import com.github.jsonzou.jmockdata.Mocker;
 
 public class LocalDateMock implements Mocker<Object> {
 
 	@Override
 	public Object mock(DataConfig mockConfig) {
-		return LocalDate.now();
+		MockConfig monthConfig = new MockConfig().intRange(1, 12);
+		MockConfig dayConfig = new MockConfig().intRange(1, 28);
+		int year = LocalDate.now().getYear();
+		int month = JMockData.mock(int.class, monthConfig);
+		int day = JMockData.mock(int.class, dayConfig);
+		return LocalDate.of(year, month, day);
 	}
 
 }
