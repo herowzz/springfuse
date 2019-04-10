fda3648738a96af0a68a4ee52ad7300f3b2acf76
herowzz <herowzz@qq.com>
2019-04-10 13:43:52 +0800

    update MockData

--- src/main/java/com/github/herowzz/springfuse/api/mock/MockData.java
+++ src/main/java/com/github/herowzz/springfuse/api/mock/MockData.java
@@ -1,5 +1,7 @@
 package com.github.herowzz.springfuse.api.mock;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,17 +11,26 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import com.github.jsonzou.jmockdata.JMockData;
+import com.github.jsonzou.jmockdata.MockConfig;
 
 public abstract class MockData {
 
+	static MockConfig config = new MockConfig();
+
+	static {
+		config.registerMocker(new LocalDateMock(), LocalDate.class);
+		config.registerMocker(new LocalDateTimeMock(), LocalDateTime.class);
+		config.intRange(0, 2);
+	}
+
 	public static <T> T mockDto(Class<T> clazz) {
-		return JMockData.mock(clazz);
+		return JMockData.mock(clazz, config);
 	}
 
 	public static <T> List<T> mockList(Class<T> clazz) {
 		List<T> list = new ArrayList<>();
 		for (int i = 0; i < 10; i++) {
-			list.add(JMockData.mock(clazz));
+			list.add(JMockData.mock(clazz, config));
 		}
 		return list;
 	}
@@ -27,7 +38,7 @@ public abstract class MockData {
 	public static <T> Page<T> mockPage(Class<T> clazz) {
 		List<T> list = new ArrayList<>();
 		for (int i = 0; i < 10; i++) {
-			list.add(JMockData.mock(clazz));
+			list.add(JMockData.mock(clazz, config));
 		}
 		Page<T> page = new PageImpl<T>(list, PageRequest.of(0, 10), 100);
 		return page;
@@ -36,7 +47,7 @@ public abstract class MockData {
 	public static <T> Page<T> mockPage(Class<T> clazz, Pageable pageable) {
 		List<T> list = new ArrayList<>();
 		for (int i = 0; i < 10; i++) {
-			list.add(JMockData.mock(clazz));
+			list.add(JMockData.mock(clazz, config));
 		}
 		Page<T> page = new PageImpl<T>(list, pageable, 100);
 		return page;
