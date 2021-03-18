055d0d0b2d26406b7b9d532e4637fe3b279dff63
wangzz <wangzz@DESKTOP-486IREP>
2021-03-18 08:45:45 +0800

    fix bug

--- src/main/java/com/github/herowzz/springfuse/core/bean/editors/format/annotation/DateTimesFormat.java
+++ src/main/java/com/github/herowzz/springfuse/core/bean/editors/format/annotation/DateTimesFormat.java
@@ -7,12 +7,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.core.annotation.AliasFor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE })
 @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-public @interface DateTimeFormat {
+public @interface DateTimesFormat {
 
 	@AliasFor(annotation = DateTimeFormat.class)
 	String pattern() default "";
